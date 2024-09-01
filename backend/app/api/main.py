5d65ec49ddc7296b4d0ab3d034219411ac71c9b9
Harkirat Singh <multaniharry714@gmail.com>
2024-09-01 05:00:21 -0400

    Improve the api response time
    Signed-off-by: Harkirat Singh <multaniharry714@gmail.com>

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -4,4 +4,4 @@ from api.routes import process, get_processed_data
 
 api_router = APIRouter()
 api_router.include_router(process.router, prefix="/process", tags=["process"])
-api_router.include_router(get_processed_data.router, prefix="/get_processed_data", tags=["get-processed"])
+api_router.include_router(get_processed_data.router, prefix="/processed_data", tags=["get-processed"])
--- backend/app/api/routes/get_processed_data.py
+++ backend/app/api/routes/get_processed_data.py
@@ -17,7 +17,6 @@ def categorize_transactions(transactions, tolerance, db: Session):
         "Negative Payout": 0,
         "Payment Pending": 0,
         "Tolerance rate breached": 0,
-       
     }
 
     today = datetime.utcnow()
@@ -49,45 +48,112 @@ def categorize_transactions(transactions, tolerance, db: Session):
 
     return categories
 
-def process_data(db: Session):
-    transaction_types = db.query(Transaction.transaction_type, func.count(Transaction.id)).group_by(Transaction.transaction_type).all()
-    payment_types = db.query(Transaction.payment_type, func.count(Transaction.id)).filter(Transaction.transaction_type == "Payment").group_by(Transaction.payment_type).all()
-
-    transactions = db.query(Transaction).all()
-    tolerance = db.query(ToleranceCheck).all()
-    categories = categorize_transactions(transactions, tolerance, db)
-
-    return {
-        "categories": categories
-    }
-
 @router.get("/")
 async def get_processed_data(db: Session = Depends(get_db)):
     try:
-        refined_data = process_data(db)
+        transaction_types = db.query(Transaction.transaction_type, func.count(Transaction.id)).group_by(Transaction.transaction_type).all()
+        payment_types = db.query(Transaction.payment_type, func.count(Transaction.id)).filter(Transaction.transaction_type == "Payment").group_by(Transaction.payment_type).all()
 
         transactions = db.query(Transaction).all()
-        blank_order = db.query(BlankOrderSummary).all()
-        tolerance_breached = db.query(ToleranceCheck).all()
+        tolerance = db.query(ToleranceCheck).all()
+        categories = categorize_transactions(transactions, tolerance, db)
 
         response = {
-            "order_payment": clean_data_for_json([t.__dict__ for t in transactions]),
-            "blank_order": clean_data_for_json([b.__dict__ for b in blank_order]),
-            "tolerance_breached": clean_data_for_json([t.__dict__ for t in tolerance_breached]),
-            "refined_data": refined_data
+            "categories": categories
         }
 
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/reim")
+@router.get("/reimbursements")
 async def get_reimbursement_summary(db: Session = Depends(get_db)):
     try:
         reimbursement_summary = db.query(ReimbursementSummary).all()
 
         response = {
-            "reimbursement_summary": clean_data_for_json([r.__dict__ for r in reimbursement_summary])
+            "reimbursements": clean_data_for_json([r.__dict__ for r in reimbursement_summary])
+        }
+
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/transactions")
+async def get_transactions(db: Session = Depends(get_db)):
+    try:
+        transactions = db.query(Transaction).filter(
+            Transaction.order_id.isnot(None),
+            Transaction.net_amount.isnot(None),
+            Transaction.invoice_amount.isnot(None)
+        )
+        
+        response = {
+            "order_payment": clean_data_for_json([t.__dict__ for t in transactions])
+        }
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/blank_orders")
+async def get_blank_orders(db: Session = Depends(get_db)):
+    try:
+        blank_orders = db.query(BlankOrderSummary).all()
+
+        response = {
+            "blank_orders": clean_data_for_json([b.__dict__ for b in blank_orders])
+        }
+
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/tolerance_breached")
+async def get_tolerance_checks(db: Session = Depends(get_db)):
+    try:
+        tolerance_checks = db.query(ToleranceCheck).all()
+
+        response = {
+            "tolerance_breached": clean_data_for_json([t.__dict__ for t in tolerance_checks])
+        }
+
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/negative_payouts")
+async def get_negative_payouts(db: Session = Depends(get_db)):
+    try:
+        negative_payouts = db.query(Transaction).filter(Transaction.net_amount < 0).all()
+
+        response = {
+            "negative_payouts": clean_data_for_json([n.__dict__ for n in negative_payouts])
+        }
+
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/payment_pending")
+async def get_payment_pending(db: Session = Depends(get_db)):
+    try:
+        payment_pending = db.query(Transaction).filter(Transaction.net_amount.is_(None)).all()
+
+        response = {
+            "payment_pending": clean_data_for_json([p.__dict__ for p in payment_pending])
+        }
+
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/returns")
+async def get_returns(db: Session = Depends(get_db)):
+    try:
+        returns = db.query(Transaction).filter(Transaction.transaction_type == "Return", Transaction.invoice_amount.isnot(None)).all()
+
+        response = {
+            "returns": clean_data_for_json([r.__dict__ for r in returns])
         }
 
         return response
